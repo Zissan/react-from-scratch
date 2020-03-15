5a0f2babc02cad09deac70ee205388ed33cfc04e
zissan <zissan.chowdhury@yahoo.com>
2020-03-15 09:59:14 +0530

    Included folder, dist, into .gitignore. Removed utility.js as it was of no use. Created a component directory under src to plcae all the components under it. This is not a good practise. But for a small application like this its ok to have this folder structure.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./components/App";
 
-render(<div>Hello, World</div>, document.getElementById("mount"));
+render(<App />, document.getElementById("mount"));
